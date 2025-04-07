a8acaf0aa53aa976a63f1bf693e28cd69950b571
gabriel <gabrielsautiere@gmail.com>
2025-04-07 15:59:38 +0200

    Test of pull from paul

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,9 +10,9 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
   const products = [
-    { id: 1, name: "Produit 1", price: 10 },
-    { id: 2, name: "Produit 2", price: 20 },
-    { id: 3, name: "Produit 3", price: 30 },
+    { id: 1, name: "Product 1", price: 10 },
+    { id: 2, name: "Product 2", price: 20 },
+    { id: 3, name: "Product 3", price: 30 },
   ];
   res.json(products);
 });
