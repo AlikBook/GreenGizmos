7328a1e90c4cf46a284c1321595c3339305b234b
AlikBook <ivanarturograndi6@gmail.com>
2025-04-13 16:10:48 +0200

    Modified the router to change the name of the tab in the browser

--- FrontEnd/src/router.js
+++ FrontEnd/src/router.js
@@ -8,18 +8,24 @@ import PrintingScanning from './views/Printing&Scanning.vue';
 import MonitorsDisplays from './views/Monitors&Displays.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/components-upgrades', component: ComponentsUpgrades },
-  { path: '/computing-equipment', component: ComputingEquipment },
-  { path: '/mobile-gear', component: MobileGear },
-  { path: '/peripherals', component: Peripherals },
-  { path: '/printing-scanning', component: PrintingScanning },
-  { path: '/monitors-displays', component: MonitorsDisplays },
+  { path: '/', component: Home, meta: { title: 'Home' } },
+  { path: '/components-upgrades', component: ComponentsUpgrades, meta: { title: 'Components & Upgrades' } },
+  { path: '/computing-equipment', component: ComputingEquipment, meta: { title: 'Computing Equipment' } },
+  { path: '/mobile-gear', component: MobileGear, meta: { title: 'Mobile Gear' } },
+  { path: '/peripherals', component: Peripherals, meta: { title: 'Peripherals' } },
+  { path: '/printing-scanning', component: PrintingScanning, meta: { title: 'Printing & Scanning' } },
+  { path: '/monitors-displays', component: MonitorsDisplays, meta: { title: 'Monitors & Displays' } },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const defaultTitle = 'GreenGizmos';
+  document.title = to.meta.title || defaultTitle;
+  next();
 });
 
 export default router;
