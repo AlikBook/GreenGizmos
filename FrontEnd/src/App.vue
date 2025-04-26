a893d600fce44f251e4ad49fe0df08717445fb12
gabriel <gabrielsautiere@gmail.com>
2025-04-26 18:00:32 +0200

    Stats vercel

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-import Menu from './components/Menu.vue';
-import Footer from './components/Footer.vue';
-
+import Menu from "./components/Menu.vue";
+import Footer from "./components/Footer.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
   <Menu></Menu>
-  <div style="margin-top: 130px;">
+  <div style="margin-top: 130px">
     <router-view></router-view>
+    <SpeedInsights />
+    <Analytics />
   </div>
   <Footer></Footer>
-  
 </template>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faCartShopping);
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
+  .component("font-awesome-icon", FontAwesomeIcon)
   .use(router)
-  .mount('#app');
+  .mount("#app");
